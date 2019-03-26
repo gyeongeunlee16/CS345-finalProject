d87ee5f7b9b68d2e9a09b883ad3a3cb64b335124
Thuy Ngoc Thai <thaingocthuy1@gmail.com>
2019-03-26 02:05:16 +0000

    index.js

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.post("/register", function(req, res){
         }
         passport.authenticate("local")(req, res, function(){
             req.flash('success', 'Welcome to YelpCamp ' + user.username + '!');
-            return res.redirect("/campgrounds"); 
+            return res.redirect("/topics"); 
         });
     });
 });
@@ -36,7 +36,7 @@ router.get("/login", function(req, res){
 //handling login logic
 router.post("/login", passport.authenticate("local", 
     {
-        successRedirect: "/campgrounds",
+        successRedirect: "/topics",
         failureRedirect: "/login"
     }), function(req, res){
 });
@@ -45,7 +45,8 @@ router.post("/login", passport.authenticate("local",
 router.get("/logout", function(req, res){
    req.logout();
    req.flash('success', 'Logged you out!');
-   return res.redirect("/campgrounds");
+   return res.redirect("/topics");
 });
 
 module.exports = router;
+
